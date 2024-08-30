40562c2328b57b4e07d56ee90dd4fd284cae6dcb
Janice Lachan <jlachan@cpp.edu>
2024-08-29 20:33:22 -0700

    First Push of NumberGuessGame

---
+++ Numbers/src/NumberGuessingGame.java
@@ -0,0 +1,35 @@
+// JavaScript source code
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberToGuess = random.nextInt(10) + 1; // Number between 1 and 10
+        int userGuess = 0;
+        int attempts = 0;
+        boolean correct = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 10:");
+
+        while (!correct) {
+            System.out.print("Your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess == numberToGuess) {
+                correct = true;
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            } else if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("Too high! Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+}
+ 
